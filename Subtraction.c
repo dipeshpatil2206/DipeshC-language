3255287d00cdf741efeea3f1783514b3fc78acf8
dipeshpatil2206 <dipeshpatil3152@gmail.com>
2025-08-20 11:32:15 +0530

    Add files via upload

---
+++ Subtraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sub;
+printf("Enter two number/n");
+scanf("%d%d",&num1,&num2);
+sub=num1 - num2;
+printf("Subtraction of %dand%d=%d\n" ,num1,num2,sub);
+getch();
+}
